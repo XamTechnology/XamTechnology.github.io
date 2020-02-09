2d10a7910ec53023df64255d3fada783c8f40aba
unknown <edklborbon@gmail.com>
2020-02-09 18:39:20 +0800

    General Idea. Landing V 0.2

---
+++ index.js
@@ -0,0 +1,27 @@
+(function() {
+    var elements;
+    var windowHeight;
+  
+    function init() {
+      elements = document.getElementsByClassName('hidden');
+      windowHeight = window.innerHeight;
+    }
+  
+    function checkPosition() {
+      for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        var positionFromTop = elements[i].getBoundingClientRect().top;
+  
+        if (positionFromTop - windowHeight <= -50) {
+          element.classList.add('animate');
+          element.classList.remove('hidden');
+        }
+      }
+    }
+  
+    window.addEventListener('scroll', checkPosition);
+    window.addEventListener('resize', init);
+  
+    init();
+    checkPosition();
+  })();
