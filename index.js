55fce017ca32f6e98ed171edeba9fa70d5b34840
unknown <edklborbon@gmail.com>
2020-02-09 21:26:14 +0800

    General Idea. Landing V 0.3

--- index.js
+++ index.js
@@ -1,20 +1,44 @@
 (function() {
-    var elements;
+    var elementsFadeInSide;
+    var elementsPopUp;
+    var elementsFadeInTop;
     var windowHeight;
   
     function init() {
-      elements = document.getElementsByClassName('hidden');
+      elementsFadeInSide = document.getElementsByClassName('element-animate-fadeinside');
+      elementsPopUp = document.getElementsByClassName('element-animate-popup');
+      elementsFadeInTop = document.getElementsByClassName('element-animate-fadeintop');
       windowHeight = window.innerHeight;
     }
   
     function checkPosition() {
-      for (var i = 0; i < elements.length; i++) {
-        var element = elements[i];
-        var positionFromTop = elements[i].getBoundingClientRect().top;
+      for (var i = 0; i < elementsFadeInSide.length; i++) {
+        var element1 = elementsFadeInSide[i];
+        var positionFromTop = elementsFadeInSide[i].getBoundingClientRect().top;
   
         if (positionFromTop - windowHeight <= -50) {
-          element.classList.add('animate');
-          element.classList.remove('hidden');
+          element1.classList.add('animate-fade-in-side');
+          element1.classList.remove('hidden');
+        }
+      }
+
+      for (var i = 0; i < elementsPopUp.length; i++) {
+        var element2 = elementsPopUp[i];
+        var positionFromTop = elementsPopUp[i].getBoundingClientRect().top;
+  
+        if (positionFromTop - windowHeight <= -50) {
+          element2.classList.add('animate-pop-up');
+          element2.classList.remove('hidden');
+        }
+      }
+
+      for (var i = 0; i < elementsFadeInTop.length; i++) {
+        var element3 = elementsFadeInTop[i];
+        var positionFromTop = elementsFadeInTop[i].getBoundingClientRect().top;
+  
+        if (positionFromTop - windowHeight <= -50) {
+          element3.classList.add('animate-fade-in-top');
+          element3.classList.remove('hidden');
         }
       }
     }
